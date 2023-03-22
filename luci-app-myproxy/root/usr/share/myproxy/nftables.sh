8b255c37371baf4e68e578e5248b2797921a7883
xuwei <xuwei@dfsoft.com.cn>
2023-03-22 10:16:59 +0800

    update nft tables

--- luci-app-myproxy/root/usr/share/myproxy/nftables.sh
+++ luci-app-myproxy/root/usr/share/myproxy/nftables.sh
@@ -37,110 +37,10 @@ add_firewall_rule() {
 
 	nft 'add table inet myproxy'
 	nft 'flush table inet myproxy'
-	gen_nftset "LOCAL_SUBNET" ipv4_addr "127.0.0.0/8, 224.0.0.0/4, 192.168.0.0/16, 10.0.0.0/8, 172.16.0.0/12"
-	if [ "$tcp_proxy_way" = "redirect" ]; then
-		# __nft=$(cat <<- EOF
-		# 	define LOCAL_SUBNET = {127.0.0.0/8, 224.0.0.0/4, 192.168.0.0/16, 10.0.0.0/8, 172.16.0.0/12}
-		# 	table myproxy
-		# 	flush table myproxy
-		# 	table myproxy {
-		# 	    # chain proxy {
-        # 		# 	ip daddr \$LOCAL_SUBNET counter accept
-        # 		# 	mark 0xff counter return
-        # 		# 	ip protocol tcp redirect to :$REDIR_PORT 
-        # 		# 	ip protocol udp tproxy to :$REDIR_PORT  meta mark set 1
-    	# 		# }
+	nft 'add set inet myproxy LOCAL_SUBNET { type ipv4_addr; flags interval; auto-merge; }'
+	nft 'add element inet myproxy LOCAL_SUBNET {127.0.0.0/8, 224.0.0.0/4, 192.168.0.0/16, 10.0.0.0/8, 172.16.0.0/12}'
 
-		# 		  chain output {
-		# 		    type nat hook output priority filter; policy accept;
-		# 		    ip daddr \$LOCAL_SUBNET return
-		# 		    ip protocol tcp redirect to :$REDIR_PORT 
-		# 		  }
-		# 		  chain prerouting {
-		# 		    type nat hook prerouting priority dstnat; policy accept;
-		# 		    ip protocol tcp redirect to :$REDIR_PORT 
-		# 		    ip protocol udp redirect to :$REDIR_PORT 
-		# 		  }
-    	# 		# chain output {
-        # 		# 	type route hook output priority 0; policy accept;
-        # 		# 	ip daddr \$LOCAL_SUBNET counter accept
-        # 		# 	meta mark 255 return
-        # 		# 	ip protocol tcp meta mark set 1
-        # 		# 	ip protocol udp meta mark set 1
-        # 		# 	jump proxy
-    	# 		# }
-    	# 		# chain filter {
-        #         #     type filter hook prerouting priority -150 ;
-        #         #     # meta l4proto tcp socket transparent 1 meta mark set 1  accept
-        #         #     jump proxy
-        #         # }		
-		# 	}			
-		# EOF
-		# )
-
-		nft 'add chain inet myproxy MY_OUTPUT'
-		nft 'add rule inet myproxy MY_OUTPUT ip daddr @LOCAL_SUBNET counter accept'
-		nft 'add rule inet myproxy MY_OUTPUT meta mark 0x000000ff counter return'
-		nft add rule inet myproxy MY_OUTPUT ip protocol tcp counter redirect to :${REDIR_PORT}
-
-		nft 'add chain inet myproxy nat_output {type nat hook output priority filter - 1; policy accept;} '
-		nft 'add rule inet myproxy nat_output ip protocol tcp counter jump MY_OUTPUT '
-
-		nft 'add chain inet myproxy MY_DIVERT'
-		nft 'add rule inet myproxy MY_DIVERT meta l4proto tcp socket transparent 1 meta mark set 0x00000001 counter accept'
-
-		nft 'add chain inet myproxy MY_REDIRECT'
-
-		nft 'add chain inet myproxy MY_RULE'
-		nft 'add rule inet myproxy MY_RULE meta mark set ct mark counter '
-		nft 'add rule inet myproxy MY_RULE meta mark 0x00000001 counter return '
-		nft 'add rule inet myproxy MY_RULE tcp flags syn / fin,syn,rst,ack meta mark set meta mark & 0x00000001 | 0x00000001 counter  '
-		nft 'add rule inet myproxy MY_RULE meta l4proto udp ct state new meta mark set meta mark & 0x00000001 | 0x00000001 counter'
-		nft 'add rule inet myproxy MY_RULE ct mark set meta mark counter '
-
-		nft 'add chain inet myproxy MY_MANGLE'
-		nft 'add rule inet myproxy MY_MANGLE ip daddr @LOCAL_SUBNET counter return'
-		nft 'add rule inet myproxy  ip protocol udp udp dport 53 counter return'
-		nft 'add rule inet myproxy MY_MANGLE udp dport { 80, 443 } ip daddr 198.18.0.0/16 counter drop'
-
-		nft 'add chain inet myproxy MY_OUTPUT_MANGLE'
-		nft 'add rule inet myproxy MY_OUTPUT_MANGLE  ip daddr @LOCAL_SUBNET counter return'
-		nft 'add rule inet myproxy MY_OUTPUT_MANGLE  meta mark 0x000000ff counter return'
-		nft 'add rule inet myproxy MY_OUTPUT_MANGLE  ip protocol udp ip daddr 198.18.0.0/16 udp dport { 80, 443 } counter drop'
-
-		nft 'add chain inet myproxy MYPROXY'
-		nft 'add rule inet myproxy MYPROXY ip daddr @LOCAL_SUBNET counter return'
-		nft add rule inet myproxy MYPROXY ip protocol tcp ip daddr 198.18.0.0/16 counter redirect to :${REDIR_PORT}
-		nft add rule inet myproxy MYPROXY ip protocol tcp counter redirect to :${REDIR_PORT}
-
-		nft 'add chain inet myproxy mangle_output {type route hook output priority mangle; policy accept;}'
-		nft 'add rule inet myproxy mangle_output oif "lo" counter return'
-		nft 'add rule inet myproxy mangle_output meta mark 0x00000001 counter return'
-
-		nft 'add chain inet myproxy mangle_prerouting {type filter hook prerouting priority mangle; policy accept;}'
-		nft 'add rule inet myproxy mangle_prerouting counter jump MY_DIVERT'
-		nft 'add rule inet myproxy mangle_prerouting meta nfproto ipv4 counter jump MY_MANGLE'
-
-		nft 'add chain inet myproxy dstnat { type nat hook prerouting priority dstnat; policy accept;} '
-		nft 'add rule inet myproxy dstnat jump MY_REDIRECT'
-		nft 'add rule inet myproxy dstnat ip protocol tcp counter jump MYPROXY'
-
-	elif [ "$tcp_proxy_way" = "tproxy" ]; then
-		echolog "tproxy mode..."
-		nft 'add chain inet myproxy proxy {type filter hook prerouting priority 0; policy accept;}'
-		nft 'add rule inet myproxy proxy ip protocol != { tcp, udp } accept'
-		nft 'add rule inet myproxy proxy ip daddr @LOCAL_SUBNET accept'
-		nft 'add rule inet myproxy proxy mark 0xff counter return'
-		nft add rule myproxy proxy meta l4proto {tcp, udp} mark set 1 tproxy to :${REDIR_PORT} counter accept
-
-		nft 'add chain inet myproxy ouput {type route hook output priority 0; policy accept;}'
-		nft 'add rule inet myproxy ouput ip daddr @LOCAL_SUBNET accept'
-		nft 'add rule inet myproxy ouput mark 255 counter return'
-		nft 'add rule inet myproxy ouput meta l4proto {tcp, udp} mark set 1 counter accept'
-
-		nft 'add chain inet myproxy filter {type filter hook prerouting priority -150 ;}'
-		nft 'add rule inet myproxy filter meta l4proto tcp socket transparent 1 meta mark set 1  accept'
-	else 
+	if [ "$tcp_proxy_way" = "tun" ]; then
 		echolog "tun mode..."
 		nft 'add chain inet myproxy local {type route hook output priority 0; policy accept;}'
 		nft 'add rule inet myproxy local ip protocol != { tcp, udp } accept'
@@ -153,8 +53,202 @@ add_firewall_rule() {
 		nft 'add rule inet myproxy forward iifname "utun" accept'
 		nft 'add rule inet myproxy forward ip daddr @LOCAL_SUBNET accept'
 		nft 'add rule inet myproxy forward mark set 255'
-
+		return
 	fi
+
+
+	nft 'add chain inet myproxy nat_output { type nat hook output priority -1; }'
+
+	nft "add chain inet myproxy PSW_DIVERT"
+	nft "flush chain inet myproxy PSW_DIVERT"
+	nft "add rule inet myproxy PSW_DIVERT meta mark 88 counter return"
+	nft "add rule inet myproxy PSW_DIVERT meta l4proto tcp socket transparent 1 mark set 1 counter accept"
+
+	nft "add chain inet myproxy PSW_REDIRECT"
+	nft "flush chain inet myproxy PSW_REDIRECT"
+
+
+	nft 'add chain inet myproxy dstnat { type nat hook prerouting priority dstnat; policy accept;} '
+	nft "add rule inet myproxy dstnat jump PSW_REDIRECT"
+
+
+	nft "add chain inet myproxy PSW_RULE"
+	nft "flush chain inet myproxy PSW_RULE"
+	nft "add rule inet myproxy PSW_RULE meta mark set ct mark counter"
+	nft "add rule inet myproxy PSW_RULE meta mark 1 counter return"
+	nft "add rule inet myproxy PSW_RULE tcp flags &(fin|syn|rst|ack) == syn meta mark set mark and 0x0 xor 0x1 counter"
+	nft "add rule inet myproxy PSW_RULE meta l4proto udp ct state new meta mark set mark and 0x0 xor 0x1 counter"
+	nft "add rule inet myproxy PSW_RULE ct mark set mark counter"
+
+	#ipv4 tproxy mode and udp
+	nft "add chain inet myproxy PSW_MANGLE"
+	nft "flush chain inet myproxy PSW_MANGLE"
+	nft "add rule inet myproxy PSW_MANGLE meta mark 88 counter return"
+	nft "add rule inet myproxy PSW_MANGLE ip daddr @LOCAL_SUBNET counter return"
+
+	nft "add chain inet myproxy PSW_OUTPUT_MANGLE"
+	nft "flush chain inet myproxy PSW_OUTPUT_MANGLE"
+	nft "add rule inet myproxy PSW_OUTPUT_MANGLE meta mark 88 counter return"
+	nft "add rule inet myproxy PSW_OUTPUT_MANGLE ip daddr @LOCAL_SUBNET counter return"
+	nft "add rule inet myproxy PSW_OUTPUT_MANGLE meta mark 0xff counter return"
+
+	# jump chains
+	nft "add chain inet myproxy mangle_prerouting { type filter hook prerouting priority mangle; policy accept; }"
+	nft "add rule inet myproxy mangle_prerouting counter jump PSW_DIVERT"
+	nft "add rule inet myproxy mangle_prerouting meta nfproto {ipv4} counter jump PSW_MANGLE"
+
+	nft 'add chain inet myproxy mangle_output {type route hook output priority mangle; policy accept;}'
+
+	#ipv4 tcp redirect mode
+	[ "$tcp_proxy_way" = "redirect" ] && {
+		nft "add chain inet myproxy PSW"
+		nft "flush chain inet myproxy PSW"
+		nft "add rule inet myproxy PSW meta mark 88 counter return"
+		nft "add rule inet myproxy PSW ip daddr @LOCAL_SUBNET counter return"
+		nft "add rule inet myproxy dstnat ip protocol tcp counter jump PSW"
+
+		nft "add chain inet myproxy PSW_OUTPUT"
+		nft "flush chain inet myproxy PSW_OUTPUT"
+		nft "add rule inet myproxy PSW_OUTPUT meta mark 88 counter return"
+		nft "add rule inet myproxy PSW_OUTPUT ip daddr @LOCAL_SUBNET counter return"
+		nft "add rule inet myproxy PSW_OUTPUT meta mark 0xff counter return"
+	}
+
+	if [ "$tcp_proxy_way" = "redirect" ]; then
+		nft "add rule inet myproxy PSW_OUTPUT ip protocol tcp counter redirect to :${REDIR_PORT}"
+		nft "add rule inet myproxy nat_output ip protocol tcp counter jump PSW_OUTPUT"
+	elif [ "$tcp_proxy_way" = "tproxy" ]; then
+		nft "add rule inet myproxy PSW_OUTPUT_MANGLE ip protocol tcp jump PSW_RULE"
+		nft "add rule inet myproxy PSW_MANGLE meta l4proto tcp iif lo tproxy to :${REDIR_PORT} counter comment \"本机\""
+		nft "add rule inet myproxy PSW_MANGLE ip protocol tcp iif lo counter return comment \"本机\""
+		nft "add rule inet myproxy mangle_output meta nfproto {ipv4} meta l4proto tcp counter jump PSW_OUTPUT_MANGLE comment \"mangle-OUTPUT-PSW\""
+	fi
+
+	nft "add rule inet myproxy PSW_OUTPUT_MANGLE ip protocol udp jump PSW_RULE"
+	nft "add rule inet myproxy PSW_MANGLE meta l4proto udp iif lo tproxy to :${REDIR_PORT} comment \"本机\""
+	nft "add rule inet myproxy PSW_MANGLE ip protocol udp iif lo counter return comment \"本机\""
+	nft "add rule inet myproxy mangle_output meta nfproto {ipv4} meta l4proto udp counter jump PSW_OUTPUT_MANGLE comment \"mangle-OUTPUT-PSW\""
+
+	nft "add rule inet myproxy mangle_output oif lo counter return comment \"mangle-OUTPUT-PSW\""
+	nft "add rule inet myproxy mangle_output meta mark 1 counter return comment \"mangle-OUTPUT-PSW\""
+	# gen_nftset "LOCAL_SUBNET" ipv4_addr "127.0.0.0/8, 224.0.0.0/4, 192.168.0.0/16, 10.0.0.0/8, 172.16.0.0/12"
+	# if [ "$tcp_proxy_way" = "redirect" ]; then
+	# 	# __nft=$(cat <<- EOF
+	# 	# 	define LOCAL_SUBNET = {127.0.0.0/8, 224.0.0.0/4, 192.168.0.0/16, 10.0.0.0/8, 172.16.0.0/12}
+	# 	# 	table myproxy
+	# 	# 	flush table myproxy
+	# 	# 	table myproxy {
+	# 	# 	    # chain proxy {
+    #     # 		# 	ip daddr \$LOCAL_SUBNET counter accept
+    #     # 		# 	mark 0xff counter return
+    #     # 		# 	ip protocol tcp redirect to :$REDIR_PORT 
+    #     # 		# 	ip protocol udp tproxy to :$REDIR_PORT  meta mark set 1
+    # 	# 		# }
+
+	# 	# 		  chain output {
+	# 	# 		    type nat hook output priority filter; policy accept;
+	# 	# 		    ip daddr \$LOCAL_SUBNET return
+	# 	# 		    ip protocol tcp redirect to :$REDIR_PORT 
+	# 	# 		  }
+	# 	# 		  chain prerouting {
+	# 	# 		    type nat hook prerouting priority dstnat; policy accept;
+	# 	# 		    ip protocol tcp redirect to :$REDIR_PORT 
+	# 	# 		    ip protocol udp redirect to :$REDIR_PORT 
+	# 	# 		  }
+    # 	# 		# chain output {
+    #     # 		# 	type route hook output priority 0; policy accept;
+    #     # 		# 	ip daddr \$LOCAL_SUBNET counter accept
+    #     # 		# 	meta mark 255 return
+    #     # 		# 	ip protocol tcp meta mark set 1
+    #     # 		# 	ip protocol udp meta mark set 1
+    #     # 		# 	jump proxy
+    # 	# 		# }
+    # 	# 		# chain filter {
+    #     #         #     type filter hook prerouting priority -150 ;
+    #     #         #     # meta l4proto tcp socket transparent 1 meta mark set 1  accept
+    #     #         #     jump proxy
+    #     #         # }		
+	# 	# 	}			
+	# 	# EOF
+	# 	# )
+
+	# 	nft 'add chain inet myproxy MY_OUTPUT'
+	# 	nft 'add rule inet myproxy MY_OUTPUT ip daddr @LOCAL_SUBNET counter accept'
+	# 	nft 'add rule inet myproxy MY_OUTPUT meta mark 0x000000ff counter return'
+	# 	nft add rule inet myproxy MY_OUTPUT ip protocol tcp counter redirect to :${REDIR_PORT}
+
+	# 	nft 'add chain inet myproxy nat_output {type nat hook output priority filter - 1; policy accept;} '
+	# 	nft 'add rule inet myproxy nat_output ip protocol tcp counter jump MY_OUTPUT '
+
+	# 	nft 'add chain inet myproxy MY_DIVERT'
+	# 	nft 'add rule inet myproxy MY_DIVERT meta l4proto tcp socket transparent 1 meta mark set 0x00000001 counter accept'
+
+	# 	nft 'add chain inet myproxy MY_REDIRECT'
+
+	# 	nft 'add chain inet myproxy MY_RULE'
+	# 	nft 'add rule inet myproxy MY_RULE meta mark set ct mark counter '
+	# 	nft 'add rule inet myproxy MY_RULE meta mark 0x00000001 counter return '
+	# 	nft 'add rule inet myproxy MY_RULE tcp flags syn / fin,syn,rst,ack meta mark set meta mark & 0x00000001 | 0x00000001 counter  '
+	# 	nft 'add rule inet myproxy MY_RULE meta l4proto udp ct state new meta mark set meta mark & 0x00000001 | 0x00000001 counter'
+	# 	nft 'add rule inet myproxy MY_RULE ct mark set meta mark counter '
+
+	# 	nft 'add chain inet myproxy MY_MANGLE'
+	# 	nft 'add rule inet myproxy MY_MANGLE ip daddr @LOCAL_SUBNET counter return'
+	# 	nft 'add rule inet myproxy  ip protocol udp udp dport 53 counter return'
+	# 	nft 'add rule inet myproxy MY_MANGLE udp dport { 80, 443 } ip daddr 198.18.0.0/16 counter drop'
+
+	# 	nft 'add chain inet myproxy MY_OUTPUT_MANGLE'
+	# 	nft 'add rule inet myproxy MY_OUTPUT_MANGLE  ip daddr @LOCAL_SUBNET counter return'
+	# 	nft 'add rule inet myproxy MY_OUTPUT_MANGLE  meta mark 0x000000ff counter return'
+	# 	nft 'add rule inet myproxy MY_OUTPUT_MANGLE  ip protocol udp ip daddr 198.18.0.0/16 udp dport { 80, 443 } counter drop'
+
+	# 	nft 'add chain inet myproxy MYPROXY'
+	# 	nft 'add rule inet myproxy MYPROXY ip daddr @LOCAL_SUBNET counter return'
+	# 	nft add rule inet myproxy MYPROXY ip protocol tcp ip daddr 198.18.0.0/16 counter redirect to :${REDIR_PORT}
+	# 	nft add rule inet myproxy MYPROXY ip protocol tcp counter redirect to :${REDIR_PORT}
+
+	# 	nft 'add chain inet myproxy mangle_output {type route hook output priority mangle; policy accept;}'
+	# 	nft 'add rule inet myproxy mangle_output oif "lo" counter return'
+	# 	nft 'add rule inet myproxy mangle_output meta mark 0x00000001 counter return'
+
+	# 	nft 'add chain inet myproxy mangle_prerouting {type filter hook prerouting priority mangle; policy accept;}'
+	# 	nft 'add rule inet myproxy mangle_prerouting counter jump MY_DIVERT'
+	# 	nft 'add rule inet myproxy mangle_prerouting meta nfproto ipv4 counter jump MY_MANGLE'
+
+	# 	nft 'add chain inet myproxy dstnat { type nat hook prerouting priority dstnat; policy accept;} '
+	# 	nft 'add rule inet myproxy dstnat jump MY_REDIRECT'
+	# 	nft 'add rule inet myproxy dstnat ip protocol tcp counter jump MYPROXY'
+
+	# elif [ "$tcp_proxy_way" = "tproxy" ]; then
+	# 	echolog "tproxy mode..."
+	# 	nft 'add chain inet myproxy proxy {type filter hook prerouting priority 0; policy accept;}'
+	# 	nft 'add rule inet myproxy proxy ip protocol != { tcp, udp } accept'
+	# 	nft 'add rule inet myproxy proxy ip daddr @LOCAL_SUBNET accept'
+	# 	nft 'add rule inet myproxy proxy mark 0xff counter return'
+	# 	nft add rule myproxy proxy meta l4proto {tcp, udp} mark set 1 tproxy to :${REDIR_PORT} counter accept
+
+	# 	nft 'add chain inet myproxy ouput {type route hook output priority 0; policy accept;}'
+	# 	nft 'add rule inet myproxy ouput ip daddr @LOCAL_SUBNET accept'
+	# 	nft 'add rule inet myproxy ouput mark 255 counter return'
+	# 	nft 'add rule inet myproxy ouput meta l4proto {tcp, udp} mark set 1 counter accept'
+
+	# 	nft 'add chain inet myproxy filter {type filter hook prerouting priority -150 ;}'
+	# 	nft 'add rule inet myproxy filter meta l4proto tcp socket transparent 1 meta mark set 1  accept'
+	# else 
+	# 	echolog "tun mode..."
+	# 	nft 'add chain inet myproxy local {type route hook output priority 0; policy accept;}'
+	# 	nft 'add rule inet myproxy local ip protocol != { tcp, udp } accept'
+	# 	nft 'add rule inet myproxy local ip daddr @LOCAL_SUBNET accept'
+	# 	nft 'add rule inet myproxy local ct state new ct mark set 255 counter'
+	# 	nft 'add rule inet myproxy local ct mark 255 mark set 255 counter'
+
+	# 	nft 'add chain inet myproxy forward {type filter hook prerouting priority 0; policy accept;}'
+	# 	nft 'add rule inet myproxy forward ip protocol != { tcp, udp } accept'
+	# 	nft 'add rule inet myproxy forward iifname "utun" accept'
+	# 	nft 'add rule inet myproxy forward ip daddr @LOCAL_SUBNET accept'
+	# 	nft 'add rule inet myproxy forward mark set 255'
+
+	# fi
 }
 
 del_firewall_rule() {
